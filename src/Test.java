d3730c807abc5859e0891b9e77519c6c2844b44d
ruan-huajun <53886515+ruan-huajun@users.noreply.github.com>
2020-06-08 15:13:24 +0800

    git@github.com:ruan-huajun/test2.git

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+/**
+ * @ClassNAME Test
+ * Description TODO
+ * Author Ruanhuajun
+ * Date 2020/6/8 14:44
+ **/
+public class Test {
+    public static void main(String[] args){
+        System.out.println("hello world!");
+    }
+}
